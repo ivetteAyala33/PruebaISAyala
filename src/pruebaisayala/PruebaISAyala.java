71c2c3b5978049d72bc5606b88bcd47f4ff14b28
ivetteAyala33 <ivette.ayala@gmail.com>
2023-10-13 22:55:07 -0500

    Inicio de commit

---
+++ src/pruebaisayala/PruebaISAyala.java
@@ -0,0 +1,16 @@
+
+package pruebaisayala;
+
+/**
+ *
+ * @author ivis3
+ */
+public class PruebaISAyala {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba IS AYALA GITHUB");
+ }   
+}
